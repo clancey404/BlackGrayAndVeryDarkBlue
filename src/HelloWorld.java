0c4bbbf49179674e1f7b3cab41d00e7277b562b2
BROWNJW21 <brownjw21@gcc.edu>
2023-03-01 13:33:25 -0500

    updated print statement udated hello world and asked user for name

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        Scanner in = new Scanner(System.in);
+        System.out.println("What is your  name?" );
+        System.out.println("Hello "+ in.next());
+
     }
 }
